5bad86acdd051edf455df0f761f7e60fb6772bf8
Zhet <larsktw@gmail.com>
2016-10-14 10:56:28 +0200

    Changes to smsApp and separate java folder

--- smsApp/app/src/main/java/zhet/smsapp/MainActivity.java
+++ smsApp/app/src/main/java/zhet/smsapp/MainActivity.java
@@ -1,138 +1,124 @@
 package zhet.smsapp;
 
-import android.Manifest;
 import android.app.SearchManager;
-import android.content.ContentResolver;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.database.Cursor;
-import android.provider.ContactsContract;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
+import android.app.SearchableInfo;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
-import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
+import zhet.smsapp.R;
 
-public class MainActivity extends AppCompatActivity {
-
-    private ListView contactNames;
-
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        Toolbar main_toolbar = (Toolbar) findViewById(R.id.main_toolbar);
-        setSupportActionBar(main_toolbar);
-        getSupportActionBar().setTitle(R.string.main_tb_title);
-        getSupportActionBar().setSubtitle(R.string.main_tb_subtitle);
-        getSupportActionBar().setIcon(R.drawable.ic_toolbar);
-        contactNames = (ListView) findViewById(R.id.contact_names);
-
-        ListView listView_main = (ListView) findViewById(R.id.listView_main);
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.Messages));
-        listView_main.setAdapter(adapter);
-
-        FloatingActionButton fabActivity = (FloatingActionButton) findViewById(R.id.fab_main);
-        fabActivity.setOnClickListener(new View.OnClickListener(){
-
-             @Override
-             public void onClick(View view) {
-                  Intent intent = new Intent(MainActivity.this, FloatingActionButtonActivity.class);
-                 startActivity(intent);
-                }
-
-            });
-
-        FloatingActionButton fabActivity2 = (FloatingActionButton) findViewById(R.id.fab2_main);
-        fabActivity2.setOnClickListener(new View.OnClickListener(){
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ContentResolver contentResolver = getContentResolver();
-                Cursor cursor = contentResolver.query(ContactsContract.Contacts.CONTENT_URI,
-                        new String[] {ContactsContract.Contacts.DISPLAY_NAME}, null, null, null);
-
-                List<String> contacts = new ArrayList<String>();
-                if(cursor.moveToFirst()) {
-                    do {
-                        contacts.add(cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)));
-                    } while (cursor.moveToNext());
-                }
-
-                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getBaseContext(), R.layout.contact_detail, R.id.name, contacts);
-                contactNames.setAdapter(adapter);
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
 
-        final int REQUEST_CODE_ASK_PERMISSIONS = 123;
-        int hasReadContactsPermission =
-                ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_CONTACTS);
-        if(hasReadContactsPermission != PackageManager.PERMISSION_GRANTED) {
-            if (!ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
-                    Manifest.permission.READ_CONTACTS)) {
-                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_CONTACTS},
-                        REQUEST_CODE_ASK_PERMISSIONS);
-                return;
-            }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        // Mikael added code below
+        ListView listView = (ListView)findViewById(R.id.listView);
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,DomainSingleton.getSingleton(this).getData());
+        listView.setAdapter(arrayAdapter);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
         }
-
-        ActivityCompat.requestPermissions(MainActivity.this,
-                new String[]{Manifest.permission.READ_CONTACTS},
-                REQUEST_CODE_ASK_PERMISSIONS);
-
-        Button sendButtonPress = (Button) findViewById(R.id.sendButton);
-        sendButtonPress.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Send melding", Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
 
-        getMenuInflater().inflate(R.menu.main_menu, menu);
+        MenuItem menuItem = menu.findItem(R.id.app_bar_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(menuItem);
 
-        SearchView searchView = (SearchView) menu.findItem(R.id.menu_search).getActionView();
-        SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
-        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        SearchManager searchManager = (SearchManager)getSystemService(Context.SEARCH_SERVICE);
+        SearchableInfo searchableInfo = searchManager.getSearchableInfo(getComponentName());
+        searchView.setSearchableInfo(searchableInfo);
 
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
-        switch (item.getItemId()) {
-
-            case R.id.menu_1:
-                Toast.makeText(MainActivity.this, "Valg 1 presset", Toast.LENGTH_SHORT).show();
-                break;
-
-            case R.id.menu_2:
-                Toast.makeText(MainActivity.this, "Valg 2 presset", Toast.LENGTH_SHORT).show();
-                break;
-
-            case R.id.menu_3:
-                Toast.makeText(MainActivity.this, "Valg 3 presset", Toast.LENGTH_SHORT).show();
-                break;
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
